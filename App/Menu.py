058e70dd1c4f7949989391a84a3444a89b469eb5
Goncalo Nascimento <68239512+Gigolino2001@users.noreply.github.com>
2021-10-01 11:46:07 +0100

    Added Name Section and Name Supermarket to Menu.py

--- App/Menu.py
+++ App/Menu.py
@@ -5,7 +5,7 @@ class Menu:
     
     def __init__(self):
         self.__options = {
-        1: 'Create a Supermarket',
+        1: 'Name Supermarket',
         2: 'Show money earned per day',
         3: 'Show costs per day',
         4: 'Show all sections',
@@ -18,46 +18,49 @@ class Menu:
             print (key, '--', self.__options[key])
 
     def start(self):
-        supermarket = None
+        supermarket = Supermarket()
         while(True):
             self.print_menu()
             option = ''
             try:
                 option = int(input('Enter your choice: '))
             except:
-                print('->Wrong input. Please enter a number between 1 and 5')
+                print('-> Wrong input. Please enter a number between 1 and 5')
             if option == 1:
-                supermarket = Supermarket()
-                print("-> Supermarket created.\n")
+                try:
+                    name_Supermarket = str(input('Enter the name of the Supermarket: '))
+                    supermarket.set_name(name_Supermarket)
+                except:
+                    print('-> Please enter a valid name.')
             elif option == 2:
-                try:
-                    print("-> " + str(supermarket.get_money_per_day()) + "\n")
-                except:
-                    print("-> You have to create a Supermarket before.\n")
+                print("-> " + str(supermarket.get_money_per_day()) + "\n")
+
             elif option == 3:
-                try:
-                    print("-> " + str(supermarket.get_costs_per_day()) + "\n")
-                except:
-                    print("-> You have to create a Supermarket before.\n")
+                print("-> " + str(supermarket.get_costs_per_day()) + "\n")
             
             elif option == 4:
-                try:
+                name = supermarket.get_name()
+                if not (isinstance(name, type(None))):
+                    print("\n↓ SUPERMARKET " + str(name).upper() + " SECTIONS ↓")
                     for section in supermarket.get_sections():
                         print("-> " + str(section.get_number()) + " - " + str(section.get_categorie()))
                     print()
-                except:
-                    print("-> You have to create a Supermarket before.\n")
+                else:
+                    print("-> Please enter the supermarket name first.")
             
             elif option == 5:
+                sections = supermarket.get_sections()
                 try:
-                   sections = supermarket.get_sections() #### Stoped Here ######
-                   sections[25].set_categorie("Oi")
+                    number_section = int(input('Enter the number of the section between 0 and '+ str(len(sections)-1) + ': '))
+                    name_section = str(input('Enter the name of the section: '))
+                    name_before = sections[number_section].get_categorie()
+                    sections[number_section].set_categorie(name_section)
+                    print('-> Changes made: ' + str(name_before) + ' ----> ' + name_section + '\n')
                 except:
-                    print("-> You have to create a Supermarket before.\n")
-
+                   print('-> Please enter valid arguments.')
 
             elif option == 6:
                 print('Thank you for using the app!"')
                 exit()
             else:
-                print('Wrong input. Please enter a number between 1 and 5')
+                print('-> Wrong input. Please enter a number between 1 and 6')
