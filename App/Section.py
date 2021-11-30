0d28d7195e06a7117ed8d8e536cf9b0c4b711259
Goncalo Nascimento <68239512+Gigolino2001@users.noreply.github.com>
2021-11-30 21:18:29 +0000

    Update Section.py

--- App/Section.py
+++ App/Section.py
@@ -1,8 +1,10 @@
+from Product import Product
 class Section:
     def __init__(self,number, categorie):
         self.__number = number
         self.__categorie = categorie
         self.__product = []
+        self.__local = "To be defined"
 
     def get_number(self):
         return self.__number
@@ -10,11 +12,17 @@ class Section:
     def get_categorie(self):
         return self.__categorie
 
+    def get_local(self):
+        return self.__local
+
     def add_product(self,Product):
         self.__product.append(Product)
-        
-    def get_product(self):
-        return self.__food
+
+    def get_products(self):
+        return self.__product
     
     def set_categorie(self, categorie):
         self.__categorie = categorie
+
+    def set_local(self, local):
+        self.__local = local
